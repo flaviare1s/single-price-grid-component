27564ae52dc170b36b38234195a8f6918b400bb5
Flávia Reis <flaviarster@gmail.com>
2024-02-01 14:14:01 -0300

    Change all

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ciano: 'hsl(179, 62%, 43%)',
+        amareloBrilho: 'hsl(71, 73%, 54%)',
+        cinzaClaro: 'hsl(204, 43%, 93%)',
+        azulCinzento: 'hsl(218, 22%, 67%)',
+        cianoClaro: 'rgba(42, 179, 177, 0.1519)'
+      }
+    },
   },
   plugins: [],
 }
